cdadce18ffbaa47862d56bd3d8ef97588c5113d6
haliltetik <htetik4@hotmail.com>
2023-03-02 18:37:27 +0300

    #1 veri tipleri oluşturuldu

---
+++ Contacs/Models/ContactData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Contacs.Models
+{
+	public class ContactData
+	{
+        public int Id { get; set; }
+        public string UUID { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string Number { get; set; }
+        public string location { get; set; }
+        public string company { get; set; }
+        public string email { get; set; }
+        public ContactData()
+		{
+		}
+	}
+}
+
